e68ee695c5afc1d2762f831c247897c859ec859f
Gubba-Jaydeep <jaydeepgubba@gmail.com>
2019-02-22 06:34:43 +0530

    models.py update1

--- EndGame/mainapp/models.py
+++ EndGame/mainapp/models.py
@@ -8,8 +8,21 @@ class CustomerDatabase:
         self.mydb = self.client["customer"]
 
 
-    def insert(self,name,phoneNumber,email,dateOfBirth,gender,password,location=None,policyNumber=None,cliendID=None):
+    def insertData(self,name,phoneNumber,email,dateOfBirth,gender,password,location=None,policyNumber=None,cliendID=None):
         mycol = self.mydb["data"]
-        #check if data already exists
-        x = mycol.insert_one({"name":name,"phoneNumber":phoneNumber,"email":email,"dateOfBirth":dateOfBirth,"gender":gender,"password":password,"location":location,"policyNumber":policyNumber,"cliendID":cliendID})
+        if mycol.find_one({"email":email})==None:
+            x = mycol.insert_one({"name":name,"phoneNumber":phoneNumber,"email":email,"dateOfBirth":dateOfBirth,"gender":gender,"password":password,"location":location,"policyNumber":policyNumber,"cliendID":cliendID})
+            return True
+        else:
+            return False
+
+    def insertHistory(self,email,time):
+        mycol = self.mydb["history"]
+        if mycol.find_one({"email":email})==None:
+            x=mycol.insert_one({"email":email,"log":[]})
+        log=mycol.find_one({"email": email})['log']
+        log.insert(0,time)
+        mycol.update_one({"email": email}, {"$set": {"log": log}})
+
+
 
