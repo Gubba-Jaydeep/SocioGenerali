f05817a9111f17425b5d51e4a4064065d335ed82
Gubba-Jaydeep <jaydeepgubba@gmail.com>
2019-02-22 11:31:27 +0530

    directory updated -- this is main file

--- EndGame/mainapp/models.py
+++ EndGame/mainapp/models.py
@@ -7,8 +7,13 @@ class CustomerDatabase:
         self.client = pymongo.MongoClient("mongodb://jd254:pass123@cluster0-shard-00-00-yftzb.mongodb.net:27017,cluster0-shard-00-01-yftzb.mongodb.net:27017,cluster0-shard-00-02-yftzb.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true")
         self.mydb = self.client["customer"]
 
+    def getData(self,filter={}):
+        mycol = self.mydb["data"]
+        for x in mycol.find(filter,{"password":0}):
+            yield x
 
-    def insertData(self,name,phoneNumber,email,dateOfBirth,gender,password,location=None,policyNumber=None,cliendID=None):
+
+    '''def insertData(self,name,phoneNumber,email,dateOfBirth,gender,password,location=None,policyNumber=None,cliendID=None):
         mycol = self.mydb["data"]
         if mycol.find_one({"email":email})==None:
             x = mycol.insert_one({"name":name,"phoneNumber":phoneNumber,"email":email,"dateOfBirth":dateOfBirth,"gender":gender,"password":password,"location":location,"policyNumber":policyNumber,"cliendID":cliendID})
@@ -23,6 +28,80 @@ class CustomerDatabase:
         log=mycol.find_one({"email": email})['log']
         log.insert(0,time)
         mycol.update_one({"email": email}, {"$set": {"log": log}})
+
+    def deleteData(self,email):
+        mycol = self.mydb["data"]
+        if mycol.find_one({"email":email})!=None:
+            x=mycol.delete_one({"email":email})
+
+    def flushHistory(self,email):
+        mycol = self.mydb["history"]
+        if mycol.find_one({"email": email}) != None:
+            mycol.update_one({"email": email}, {"$set": {"log": []}})
+
+    def updateData(self,email,key,value):
+        mycol = self.mydb["data"]
+        if mycol.find_one({"email": email}) != None:
+            mycol.update_one({"email": email}, {"$set": {key:value}})
+    
+    '''
+
+
+
+
+
+class AdvisorDatabase:
+    def __init__(self):
+        self.client = pymongo.MongoClient("mongodb://jd254:pass123@cluster0-shard-00-00-yftzb.mongodb.net:27017,cluster0-shard-00-01-yftzb.mongodb.net:27017,cluster0-shard-00-02-yftzb.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true")
+        self.mydb = self.client["advisor"]
+
+
+    def getData(self,filter={}):
+        mycol = self.mydb["data"]
+        res={}
+        for x in mycol.find(filter,{"email":1,"password":1}):
+            res[x["email"]]=x["password"]
+
+        return res
+
+
+    #unnecessary things by jd below
+    '''def insertData(self, name, phoneNumber, email, dateOfBirth,type, gender, password, location=None):
+        mycol = self.mydb["data"]
+        if mycol.find_one({"email": email}) == None:
+            x = mycol.insert_one(
+                {"name": name, "phoneNumber": phoneNumber, "email": email, "dateOfBirth": dateOfBirth,"type":type ,"gender": gender,
+                 "password": password, "location": location})
+            return True
+        else:
+            return False
+
+    def insertHistory(self, email, time):
+        mycol = self.mydb["history"]
+        if mycol.find_one({"email": email}) == None:
+            x = mycol.insert_one({"email": email, "log": []})
+        log = mycol.find_one({"email": email})['log']
+        log.insert(0, time)
+        mycol.update_one({"email": email}, {"$set": {"log": log}})
+
+    def deleteData(self, email):
+        mycol = self.mydb["data"]
+        if mycol.find_one({"email": email}) != None:
+            x = mycol.delete_one({"email": email})
+
+    def flushHistory(self, email):
+        mycol = self.mydb["history"]
+        if mycol.find_one({"email": email}) != None:
+            mycol.update_one({"email": email}, {"$set": {"log": []}})
+
+    def updateData(self,email,key,value):
+        mycol = self.mydb["data"]
+        if mycol.find_one({"email": email}) != None:
+            mycol.update_one({"email": email}, {"$set": {key:value}})
+    '''
+
+
+
 
 
 
--- EndGame/mainapp/views.py
+++ EndGame/mainapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.template import loader
+
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'mainapp/sih/login form/login.html', {})
+
+def dashboard(request):
+    #retrieving data from form and validation and redirection to dashboard page
+    pass
