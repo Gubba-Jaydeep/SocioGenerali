58628524863f5726d26010d5ca0de3ec0728ab09
Gubba-Jaydeep <jaydeepgubba@gmail.com>
2019-02-24 17:52:37 +0530

    models.py added getCompleteInfo for advisor

--- EndGame/mainapp/models.py
+++ EndGame/mainapp/models.py
@@ -57,13 +57,22 @@ class AdvisorDatabase:
         self.client = pymongo.MongoClient("mongodb://jd254:pass123@cluster0-shard-00-00-yftzb.mongodb.net:27017,cluster0-shard-00-01-yftzb.mongodb.net:27017,cluster0-shard-00-02-yftzb.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true")
         self.mydb = self.client["advisor"]
 
-
+    #this gives a dicionary containing all advisors with key as email and password as value
     def getData(self):
         mycol = self.mydb["data"]
         res={}
         for x in mycol.find({},{"email":1,"password":1}):
             res[x["email"]]=x["password"]
         return res
+
+    #returns a dict containing the details of the user whose email is passed as paramater
+    def getCompleteInfo(self,email):
+        mycol=self.mydb["data"]
+        res=mycol.find_one({"email":email},{"_id":0,"password":0})
+        if res==None:
+            return False
+        return res
+
 
 
 
