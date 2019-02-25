54d0fa4bb008de212ddbc30ac5885b2e23f06618
Gubba-Jaydeep <jaydeepgubba@gmail.com>
2019-02-25 14:39:38 +0530

    session added

--- EndGame/mainapp/views.py
+++ EndGame/mainapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.template import loader
 from .models import AdvisorDatabase, CustomerDatabase
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -8,13 +9,17 @@ def index(request):
     return render(request, 'mainapp/login.html', {})
 
 def login(request):
+    '''retrieving data from form and validation and redirection to dashboard page'''
     db = AdvisorDatabase()
     dict_of_officials = db.getData()
     uname = request.GET['username']
     psw = request.GET['pass']
     try:
         if dict_of_officials[uname] == psw:
-            return render(request, 'mainapp/home.html', {'uname': uname})
+            response=render(request, 'mainapp/home.html', {'uname': uname})
+            response.set_cookie("loggedIn",True)
+            response.set_cookie("userName", uname)
+            return response
         else:
             return render(request, 'mainapp/index.html', {})
     except Exception as ae:
@@ -22,9 +27,20 @@ def login(request):
         return render(request, 'mainapp/error.html', ctx)
 
 def dashboard(request):
-    #retrieving data from form and validation and redirection to dashboard page
-    with open("./mklog.txt","wt") as f:
-        f.write(str(request))
+
+    '''with open("./mklog.txt","wt") as f:
+        f.write(str(request))'''
+    if request.COOKIES.get("loggedIn"):
+        return render(request, 'mainapp/home.html', {'uname': request.COOKIES.get("userName")})
+    else:
+        return render(request, 'mainapp/login.html', {})
+
+def logout(request):
+    request.COOKIES.clear()
+
+
+
+
 
 
 def myCustomers(request):
